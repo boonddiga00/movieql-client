66bf1dae8f3b579d68ff9740e35a97330d742868
이중민 <boonddiga00@naver.com>
2022-06-19 16:06:43 +0900

    Write Cache Data

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -7,23 +7,44 @@ const GET_MOVIE = gql`
       id
       title
       large_cover_image
+      isLiked @client
     }
   }
 `;
 
 function Movie() {
   const { id } = useParams();
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   });
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked,
+      },
+    });
+  };
   return loading ? (
     <h1>Fetching Data...</h1>
   ) : (
     <div>
-      <h1>{data.movie.title}</h1>
-      <img src={data.movie.large_cover_image} alt="Movie Poster" />
+      <h1>{data?.movie?.title}</h1>
+      <button onClick={onClick}>
+        {data?.movie?.isLiked ? "Unlike" : "Like"}
+      </button>
+      <img src={data?.movie?.large_cover_image} alt="Movie Poster" />
     </div>
   );
 }
