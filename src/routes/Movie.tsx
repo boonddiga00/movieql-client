1edbe62b9c4675c231e949c8628430b308bba321
이중민 <boonddiga00@naver.com>
2022-06-19 15:32:20 +0900

    Get Movie with useQuery variables

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,5 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      large_cover_image
+    }
+  }
+`;
+
 function Movie() {
-  return <div>Movie detail</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  return loading ? (
+    <h1>Fetching Data...</h1>
+  ) : (
+    <div>
+      <h1>{data.movie.title}</h1>
+      <img src={data.movie.large_cover_image} alt="Movie Poster" />
+    </div>
+  );
 }
 
 export default Movie;
