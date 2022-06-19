ff0353d222ed36028f9cc01583f2b69f69720e9c
이중민 <boonddiga00@naver.com>
2022-06-19 15:01:03 +0900

    Fetching Data With useQuery

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,29 +1,27 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
 
 function Movies() {
-  const client = useApolloClient();
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              title
-              id
-            }
-          }
-        `,
-      })
-      .then((result) => setMovies(result.data.allMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Cannot Fetch T.T</div>;
   return (
-    <ul>
-      {movies.map((movie: { id: number; title: string }) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-    </ul>
+    <>
+      <h1>Movies</h1>
+      <ul>
+        {data.allMovies.map((movie: { id: number; title: string }) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
